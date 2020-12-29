3254ab3bec7eb3422188dff21b5cf9e2fad8d981
wenjing-bonnie <liuwenjing.12@163.com>
2020-12-29 14:53:22 +0800

    add shape

---
+++ libs/WidgetPlaceholderlibrary/src/main/java/com/android/widgetplaceholder/view/PlaceHolderAnimationBackground.java
@@ -0,0 +1,26 @@
+package com.android.widgetplaceholder.view;
+
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+
+/**
+ * Created by wenjing.liu on 2020/12/7 in J1.
+ * <p>
+ * 设置预占位符含有圆角
+ *
+ * @author wenjing.liu
+ */
+public class PlaceHolderAnimationBackground extends ShapeDrawable {
+
+    public PlaceHolderAnimationBackground() {
+
+    }
+
+
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+    }
+}
